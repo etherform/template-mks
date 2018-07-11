da2a76cde0bda7deb379909632f61ad2ba033745
gvv <crutchmaster@yandex.ru>
2018-07-11 11:03:13 +0700

    Input commands in json. Stacktrace for commands exceptions

--- src/main/java/com/ric/mks/MksControl.java
+++ src/main/java/com/ric/mks/MksControl.java
@@ -1,13 +1,20 @@
 package com.ric.mks;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Vector;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MksControl<T> {
 
     private CommandReturn ret = new CommandReturn();
     private HashMap<String, Method> commandsList;
+    private ObjectMapper om = new ObjectMapper();
 
     public T commands;
 
@@ -23,27 +30,41 @@ public class MksControl<T> {
     }
 
     public CommandReturn control(String msg) {
-
-        String[] cmd = msg.split(" ");
-
-        String commandStr = cmd[0];
-        String[] args = Arrays.copyOfRange(cmd, 1, cmd.length);
+        String commandStr = null;
+        Vector<String> args = new Vector<String>();
+        try {
+            JsonNode in = om.readTree(msg);
+            commandStr = in.fieldNames().next();
+            if (in.get(commandStr).isArray()) {
+                for(JsonNode arg : in.get(commandStr)) {
+                    args.add(arg.asText());
+                }
+            }
+        } catch (Exception e) {
+            return ret.err("Json parsing error. Json is:"+msg);
+        }
 
         Method m = commandsList.get(commandStr);
         if (m == null) return ret.err("Command not found:"+commandStr);
 
         int c = m.getParameterCount();
-        if (c <= args.length) {
+        if (c <= args.size()) {
             ret.err("Error");
             try {
-                args = Arrays.copyOfRange(args, 0, c);
-                ret = (CommandReturn)m.invoke(commands, (Object[])args);
+                ret = (CommandReturn)m.invoke(commands,
+                        Arrays.copyOfRange(args.toArray(), 0, c)
+                        );
             } catch (Exception e) {
-                return ret.err("Error when calling command:"+e.getMessage());
+                StringWriter errors = new StringWriter();
+                e.printStackTrace(new PrintWriter(errors));
+                String trace = errors.toString();
+                return ret.err("Error when calling "+commandStr+":"
+            +e.getClass().getName()+" - "+e.getMessage()
+            +"\nStackTrace:\n"+trace);
             }
             return ret;
         }
-        return ret.err("Need more args for "+commandStr+"("+c+" vs "+args.length+")");
+        return ret.err("Need more args for "+commandStr+"("+c+" vs "+args.size()+")");
     }
 
 }
